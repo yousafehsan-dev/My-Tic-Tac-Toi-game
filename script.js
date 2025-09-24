adfdcc6e73be6878991e6a7e1c79eb0b17c3d1be
yousafehsan-dev <yousafehsan125@gmail.com>
2025-09-24 22:57:02 +0500

    style.css

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log('my tic tac toe')
+let  audioturn = new Audio('play.mp3.wav')
+let over = new Audio("over.mp3.wav")
+let music = new Audio("play.mp3.wav")
+let gameover = false
+
+let turn = "x"
+// function to change the turn 
+const changeturn =()=>{
+    return turn === "x"? "0":"x"
+}
+// function to check for a win 
+const checkwin =()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+let wins = [
+    [0,1,2,5,5,0],
+    [3,4,5,5,15,0],
+    [6,7,8,5,25,0],
+    [0,3,6,-5,15,90],
+    [1,4,7,5,15,90],
+    [2,5,8,15,15,90],
+    [0,4,8,5,15,45],
+    [2,4,6,5,15,135],
+]
+wins.forEach(e=>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + "  won"
+        gameover = true
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '240px';
+        document.querySelector(".line").style.width = "20vw";
+        document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw ) rotate(${e[5]}deg)`
+        over.play()
+    }
+    
+})
+}
+
+// game logic 
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click', ()=>{
+    if(boxtext.innerText===""){
+        boxtext.innerText= turn;
+       turn = changeturn();
+        audioturn.play();
+        checkwin();
+        if(!gameover){
+
+            document.getElementsByClassName("info")[0].innerText = "turn of "+ turn;
+        }
+    }
+    })
+
+});
+// update btn 
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext')
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText = ""
+    })
+    turn = "x"
+    gameover = false
+    document.querySelector(".line").style.width = "0vw";
+     document.getElementsByClassName("info")[0].innerText = "turn of "+ turn;
+     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '0px'
+      
+})
